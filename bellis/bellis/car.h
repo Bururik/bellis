efb773781c032f13b4ec81e13cfa8838e5c50010
Leonardo I. Escandon Jackson <spacepopeleo@gmail.com>
2018-08-02 22:57:52 -0500

    converted project into Visual Studio Solution

---
+++ bellis/bellis/car.h
@@ -0,0 +1,57 @@
+#pragma once
+/*
+Sample code:
+```
+struct Pet {
+public:
+    Pet(const std::string &name) : name(name) { }
+    void setName(const std::string &name_) { name = name_; }
+    const std::string &getName() const { return name; }
+private:
+    std::string name;
+};
+```
+attributes for car type
+    model
+    top speed
+    acceleration
+*/
+
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+#import <iostream>
+#import <string>
+#import "components.h"
+
+class Car {
+public:
+    // turn into inhereted constructor
+    Car(std::string model, int speed, int accel)
+    : MyCarModel(model), MyTopSpeed(speed), MyAcceleration(accel)
+    {  }
+    // add getter and setter
+
+private:
+    std::string MyCarModel;
+    int MyTopSpeed;
+    int MyAcceleration;
+};
+
+#endif
+
+
+// class Birthday {
+//     public:
+//         Birthday(int m, int d, int y)
+//         : month(m), day(d), year(y)
+//         {  }
+//         void printDate()
+//         {
+//             cout<<month<<"/"<<day <<"/"<<year<<endl;
+//         }
+//     private:
+//         int month;
+//         int day;
+//         int year;
+// };
