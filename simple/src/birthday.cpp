80627df168bef56b29a7651886db0d90aed722d3
Bururik <spacepopeleo@gmail.com>
2018-08-06 18:37:00 -0500

    made the project structure more formal as well as improved the make file

---
+++ simple/src/birthday.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "birthday.h"
+
+Birthday::Birthday(int m, int d, int y)
+: month(m), day(d), year(y) {  }
+
+void Birthday::printDate()
+{
+    std::cout<<month<<"/"<<day <<"/"<<year<<std::endl;
+}
